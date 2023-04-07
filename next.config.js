3cdb91ace4f6e7e291c0b69dab47f851ced70ea6
t1-research <121369871+t1-research@users.noreply.github.com>
2023-04-06 20:28:38 -0400

    udp

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
-
-const withImages = require('next-images')
-module.exports = withImages({
-    webpack(config, options) {
-        return config
-    }
-})

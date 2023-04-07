afd079e63959b9cebd90ba90fbcd1bf0bba28cd9
t1-research <121369871+t1-research@users.noreply.github.com>
2023-04-06 20:26:29 -0400

    tsconfig

--- next.config.js
+++ next.config.js
@@ -1,2 +1,9 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+
+const withImages = require('next-images')
+module.exports = withImages({
+    webpack(config, options) {
+        return config
+    }
+})
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import proPic from "../imgs/proPic.jpeg";
 
 export default function Document() {
   const meta = {

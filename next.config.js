e1d02965b55bb12ed93813ce6858a82aa1b5db29
t1-research <121369871+t1-research@users.noreply.github.com>
2023-04-06 20:54:08 -0400

    img?

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+
+module.exports = {
+    images: {
+      formats: ['image/png','image/jpeg', 'image/jpg'],
+    },
+  }

da4aa69d76da0d17bf2b15ccd3e36eb84b55c432
t1-research <121369871+t1-research@users.noreply.github.com>
2023-04-06 21:02:19 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,2 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
-
-module.exports = {
-    images: {
-      path: 'public/static/images/',
-    },
-  }
